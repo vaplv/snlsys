a2b1af84a8b85fc68ee784a659cd34dc0862c9a2
Vincent Forest <vaplv@free.fr>
2013-02-23 10:55:40 +0100

    Change the name of the library from sys to snlsys
    The old `sys' name was confusing with system specific headers and
    libraries. We also replace the `per file' header license by an unique
    LICENSE file convering the whole project.

--- src/clock_time.h
+++ src/clock_time.h
@@ -1,29 +1,3 @@
-/*
- * Copyright (c) 2013 Vincent Forest
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
- * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 #ifndef TIME_H
 #define TIME_H
 
@@ -55,7 +29,7 @@
   typedef struct timespec timeval_t;
 #endif
 
-#include "sys.h"
+#include "snlsys.h"
 #include <stddef.h>
 
 enum time_unit {
@@ -111,12 +85,12 @@ time_add(struct time* res, const struct time* a, const struct time* b)
 extern "C" {
 #endif
 
-SYS_API int64_t
+SNLSYS_API int64_t
 time_val
   (const struct time* time,
    enum time_unit unit);
 
-SYS_API void
+SNLSYS_API void
 time_dump
   (const struct time* time,
    int flag,
--- src/snlsys.h
+++ src/snlsys.h
@@ -1,31 +1,5 @@
-/*
- * Copyright (c) 2013 Vincent Forest
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
- * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-#ifndef SYS_H
-#define SYS_H
+#ifndef SNLSYS_H
+#define SNLSYS_H
 
 #ifndef __GNUC__
   #error "Unsupported compiler"
@@ -56,10 +30,10 @@
 #define IMPORT_SYM
 #define LOCAL_SYM __attribute__((visibility("hidden")))
 
-#if defined(SYS_SHARED_BUILD)
-  #define SYS_API EXPORT_SYM
+#if defined(SNLSYS_SHARED_BUILD)
+  #define SNLSYS_API EXPORT_SYM
 #else
-  #define SYS_API IMPORT_SYM
+  #define SNLSYS_API IMPORT_SYM
 #endif
 
 /*******************************************************************************
@@ -101,24 +75,23 @@
 #define STATIC_ASSERT(condition, msg) \
   char STATIC_ASSERT_##msg[1 -  2*(!(condition))] UNUSED
 
-#define FATAL(msg) \
-  { \
-    fprintf(stderr, msg); \
-    exit(-1); \
+#define FATAL(msg)                                                             \
+  {                                                                            \
+    fprintf(stderr, msg);                                                      \
+    exit(-1);                                                                  \
   } (void)0
 
-#define CHECK(a, b) \
-  { \
-    if((a) != (b)) \
-      FATAL("error:" STR( __FILE__ )":"STR( __LINE__ )"\n"); \
+#define CHECK(a, b)                                                            \
+  {                                                                            \
+    if((a) != (b))                                                             \
+      FATAL("error:" STR( __FILE__ )":"STR( __LINE__ )"\n");                   \
   } (void)0
 
-#define NCHECK(a, b) \
-  { \
-    if((a) == (b)) \
-      FATAL("error:" STR( __FILE__ )":"STR( __LINE__ )"\n"); \
+#define NCHECK(a, b)                                                           \
+  {                                                                            \
+    if((a) == (b))                                                             \
+      FATAL("error:" STR( __FILE__ )":"STR( __LINE__ )"\n");                   \
   } (void)0
-
 
 /*******************************************************************************
  *
@@ -170,10 +143,10 @@
 #define FORMAT_PRINTF(a, b) \
   __attribute__((format(printf, a, b)))
 
-#define IS_MEMORY_OVERLAPPED(d0, sz0, d1, sz1) \
-  (((uintptr_t)(d0) >= (uintptr_t)(d1) && \
-   (uintptr_t)(d0) < ((uintptr_t)(d1) + (sz1))) || \
-  (((uintptr_t)(d0) + (sz0)) >= (uintptr_t)(d1) && \
+#define IS_MEMORY_OVERLAPPED(d0, sz0, d1, sz1)                                 \
+  (((uintptr_t)(d0) >= (uintptr_t)(d1) &&                                      \
+   (uintptr_t)(d0) < ((uintptr_t)(d1) + (sz1))) ||                             \
+  (((uintptr_t)(d0) + (sz0)) >= (uintptr_t)(d1) &&                             \
    ((uintptr_t)(d0) + (sz0)) < ((uintptr_t)(d1) + (sz1))))
 
 #define STR__(x) #x
@@ -181,5 +154,5 @@
 
 #define UNUSED __attribute__((unused))
 
-#endif /* SYS_H */
+#endif /* SNLSYS_H */
 
